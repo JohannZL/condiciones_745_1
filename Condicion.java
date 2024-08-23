378b61a64baae0cf70ddcacb61f1877a3db22275
JohannZL <jodzambranol@udistrital.edu.co>
2024-08-23 13:52:19 -0500

    mal

--- Condicion.java
+++ Condicion.java
@@ -2,19 +2,34 @@ import java.util.Scanner;
 
 class Condicion {
   public static void main(String[] args) {
-    Scanner leer = new Scanner(System.in);
-    System.out.println("Ingrese un numero");
-    int n1 = leer.nextInt();
-    System.out.println("Ingrese otro numero");
-    int n2 = leer.nextInt();
+    String name;
+    int dia,mes,dia_ac=23,mes_ac=8,naci,year=2023,edad;
 
-    if(n1>n2)
+    Scanner leer = new Scanner(System.in);
+    System.out.println("ingrese su nombre: ");
+    name = leer.next();
+    System.out.println(name+"ingrese su year de nacimiento");
+    naci = leer.nextInt();
+    System.out.println(name+"Ingrese su mes de nacimiento: ");
+    mes = leer.nextInt();
+    System.out.println(name+"ingrese se dia de nacimiento: ");
+    dia = leer.nextInt();
+    edad = year - naci;
+    if(mes-mes_ac<0)
     {
-      System.out.println("El numero: "+n1+" es el mayor");
+      edad--;
+	System.out.println(name+"su edad es:"+edad+);
     }
-    else
+    if(dia>=21 || dia<=19) 
     {
-      System.out.println("El numero: "+n2+" es el mayor");
+      if(mes>=3 || mes<=4){
+	System.out.println(name+"su signo zodiacal es Aries");
+	}
     }
-  }
+	if(dia>=20 || dia<=20){
+		if(mes>=4 || mes<=5){
+		System.out.println(name+"su signo zodiacal es Tauro");
+			}
+		}  
+}
 }
